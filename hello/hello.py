7080a965377e52814ea0eafcd2ebe53d51e9be7d
s2012975 <bdips2012@gmail.com>
2021-01-29 01:19:33 +0900

    [start]shoot

--- hello/hello.py
+++ hello/hello.py
@@ -1,2 +1,7 @@
+#普通の Python プログラムとして実行する方法
+import pgzrun
 def draw():
+    name = "martin"
     screen.draw.text("Hello", topleft=(10, 10))
+    screen.draw.text(name, topleft=(20, 20))
+pgzrun.go()
