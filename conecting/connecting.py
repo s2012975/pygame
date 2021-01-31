f435d88c4eab52bf6243c285bc487d127ba40abd
s2012975 <bdips2012@gmail.com>
2021-01-31 21:41:22 +0900

    [finish]banana_connecting

--- conecting/connecting.py
+++ conecting/connecting.py
@@ -6,7 +6,7 @@ height = 400
 bananas = []
 lines = []
 
-next_dot = 0
+next_banana = 0
 
 for banana in range(0, 10):
     actor = Actor("banana")
@@ -15,9 +15,23 @@ for banana in range(0, 10):
 
 
 def draw():
-    screen.fill("black")
+    screen.fill("gray")
     number = 1
     for banana in bananas:
         screen.draw.text(str(number), (banana.pos[0], banana.pos[1] + 12))
         banana.draw()
         number = number + 1
+    for line in lines:
+        screen.draw.line(line[0], line[1], (100, 0, 0))
+
+
+def on_mouse_down(pos):
+    global next_banana
+    global lines
+    if bananas[next_banana].collidepoint(pos):
+        if next_banana:
+            lines.append((bananas[next_banana - 1].pos, bananas[next_banana].pos))
+        next_banana += 1
+    else:
+        lines = []
+        next_banana = 0
